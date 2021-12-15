a08982fe539ae1e7832cc929f0f5a4f18e2ce1e9
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-12-15 14:58:00 -0800

    dst 1 first

--- mktime_bug.py
+++ mktime_bug.py
@@ -23,5 +23,5 @@ mktime.argtypes = [ctypes.POINTER(TimeStruct)]
 mktime.restype = ctypes.c_int
 
 tm = TimeStruct(tm_year=2017, tm_mon=5, tm_mday=21, tm_hour=15, tm_min=30, tm_sec=16, tm_wday=6, tm_yday=141, tm_isdst=dst)
-res = mktime(tm)
+res = mktime(ctypes.byref(tm))
 print(f"tm_isdst={dst} mktime -> {res}", flush=True)
